2bf7191c18c093bb2b7888de3b79fe4a930d8788
Daniel Weber <danielcweber@users.noreply.github.com>
2023-10-13 22:06:08 +0200

    Add a second test project.

---
+++ tests/Tests2/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace IKVMvNextRepro.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var c = new Class1();
+
+            c.Method();
+        }
+    }
+}
