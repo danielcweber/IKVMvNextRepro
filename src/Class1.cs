0062e853402dc2d40882abea770462422b96c6ff
Daniel Weber <danielcweber@users.noreply.github.com>
2023-10-13 20:45:31 +0200

    Initial commit.

---
+++ src/Class1.cs
@@ -0,0 +1,11 @@
+﻿using org.apache.commons.configuration2;
+
+namespace IKVMvNextRepro;
+
+public class Class1
+{
+    public void Method()
+    {
+        var conf = new BaseConfiguration();
+    }
+}
